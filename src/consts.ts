06f2ffc350d3de6d5cbf3b0122d12d24b362b68d
BwireRodneyDesire <Bwirerodnery3@gmai.com>
2026-03-13 16:52:08 +0300

    new work

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Bwire Rodney | Developer Portfolio";
+export const SITE_DESCRIPTION = "Portfolio of Bwire Rodney - full-stack developer and UI/UX-focused builder.";
